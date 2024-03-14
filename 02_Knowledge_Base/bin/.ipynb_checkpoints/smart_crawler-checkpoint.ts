f4e519a68fe6f201c29078ab9adc661aae9a1b28
zanbei <zhaoanbei@outlook.com>
2024-03-14 10:11:21 +0800

    clean

--- 02_Knowledge_Base/bin/.ipynb_checkpoints/smart_crawler-checkpoint.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { SmartCrawlerStack } from '../lib/smart_crawler-stack';
-
-const app = new cdk.App();
-new SmartCrawlerStack(app, 'SmartCrawlerStack');
